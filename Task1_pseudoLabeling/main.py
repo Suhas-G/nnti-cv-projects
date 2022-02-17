5eac06cd582c373ea01a5652a661282ac9148db5
Suhas-G <suhas.g96.sg@gmail.com>
2022-02-17 22:16:48 +0100

    task1: validation + lr scheduler + condor

--- Task1_pseudoLabeling/main.py
+++ Task1_pseudoLabeling/main.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python3
 import argparse
+import datetime
 import math
+import random
 
-from dataloader import get_cifar10, get_cifar100
-from utils      import accuracy
-
-from model.wrn  import WideResNet
-
+import numpy as np
 import torch
 import torch.optim as optim
-from torch.utils.data   import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torch.utils.tensorboard import SummaryWriter
 
+torch.manual_seed(42)
+random.seed(42)
+np.random.seed(42)
+
+from dataloader import get_cifar10, get_cifar100
+from model.wrn import WideResNet
+from utils import accuracy, load_checkpoint, save_checkpoint
+
+current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 
 class PseudoLabelledDataset(torch.utils.data.Dataset):
     def __init__(self) -> None:
@@ -32,6 +40,46 @@ class PseudoLabelledDataset(torch.utils.data.Dataset):
             self.data = torch.concat([self.data, data.detach().clone()])
             self.targets = torch.concat([self.targets, target.detach().clone()])
 
+def create_validation_dataset(unlabelled_train, num_classes, size=1000):
+    class_size = size // num_classes
+    validation_idx = []
+    for c in range(num_classes):
+        c_targets = np.where(unlabelled_train.targets == c)[0]
+        assert len(c_targets) >= class_size
+        idx = np.random.choice(c_targets, class_size, replace=False)
+        validation_idx.extend(idx)
+    
+    return Subset(unlabelled_train, validation_idx)
+
+
+def validate(model: torch.nn.Module, val_loader: DataLoader, 
+            criterion: torch.nn.CrossEntropyLoss, device: torch.device):
+    model.eval()
+    val_loss = 0.0
+    val_acc = 0.0
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            val_loss += criterion(output, target).item()
+            val_acc += accuracy(output, target)[0]
+    return val_loss / len(val_loader), val_acc / len(val_loader)
+
+def get_params(args):
+    return {
+        'dataset': args.dataset,
+        'num_labeled': args.num_labeled,
+        'lr': args.lr,
+        'momentum': args.momentum,
+        'weight_decay': args.wd,
+        'train_batch_size': args.train_batch,
+        'test_batch_size': args.test_batch,
+        'total_iter': args.total_iter,
+        'iter_per_epoch': args.iter_per_epoch,
+        'threshold': args.threshold,
+        'model_depth': args.model_depth,
+        'model_width': args.model_width
+    }
 
 
 def main(args):
@@ -43,9 +91,12 @@ def main(args):
         args.num_classes = 100
         labeled_dataset, unlabeled_dataset, test_dataset = get_cifar100(args, 
                                                                 args.datapath)
+
+    validation_dataset = create_validation_dataset(unlabeled_dataset, args.num_classes, size=10000)
     args.epoch = math.ceil(args.total_iter / args.iter_per_epoch)
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Device: ', device)
 
     writer = SummaryWriter(log_dir=args.dataout)
 
@@ -61,9 +112,15 @@ def main(args):
                                     batch_size = args.test_batch,
                                     shuffle = False, 
                                     num_workers=args.num_workers)
+    val_loader          = DataLoader(validation_dataset, batch_size=args.test_batch,
+                                    shuffle = True, num_workers=args.num_workers)
     
     model       = WideResNet(args.model_depth, 
                                 args.num_classes, widen_factor=args.model_width)
+    start_epoch = 0
+    if args.resume:
+        rets = load_checkpoint(args.model_path, model = model, return_optimiser=True)
+        start_epoch = rets['epoch'] + 1
     model       = model.to(device)
 
     ############################################################################
@@ -71,9 +128,12 @@ def main(args):
     ############################################################################
 
     optimiser = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    if args.resume:
+        optimiser.load_state_dict(rets['optimiser'])
     criterion = torch.nn.CrossEntropyLoss()
+    scheduler = optim.lr_scheduler.ExponentialLR(optimiser, gamma=args.lr_decay)
     
-    for epoch in range(args.epoch):
+    for epoch in range(start_epoch, start_epoch + args.epoch):
         model.train()
         pseudo_labelled = PseudoLabelledDataset()
         for i in range(args.iter_per_epoch):
@@ -113,15 +173,25 @@ def main(args):
                 outputs = model(x_ul)
                 pseudo_x = x_ul[(outputs >= args.threshold).any(axis=1)]
                 _, pseudo_label = torch.max(outputs[(outputs >= args.threshold).any(axis=1)], axis=1)
-                pseudo_labelled.append(pseudo_x, pseudo_label)
+                pseudo_labelled.append(pseudo_x.cpu(), pseudo_label.cpu())
 
-        labeled_dataset = torch.utils.data.ConcatDataset([labeled_dataset, pseudo_labelled])
+        if epoch >= 1:
+            labeled_dataset = torch.utils.data.ConcatDataset([labeled_dataset, pseudo_labelled])
 
+        validation_loss, validation_accuracy = validate(model, val_loader, criterion, device)
+        writer.add_scalar('loss/validation', validation_loss, epoch)
+        writer.add_scalar('accuracy/validation', validation_accuracy, epoch)
+
+        scheduler.step()
+        if epoch == 0 or (epoch + 1) % args.checkpoint_freq == 0:
+            save_checkpoint(model, epoch, './checkpoints/{}/checkpoint_{}.pth'.format(current_time,epoch + 1),
+                            optimiser = optimiser, params=get_params(args))
     writer.close()
 
 
 
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser(description="Pseudo labeling \
                                         of CIFAR10/100 with pytorch")
     parser.add_argument("--dataset", default="cifar10", 
@@ -136,6 +206,8 @@ if __name__ == "__main__":
                         help="Optimizer momentum")
     parser.add_argument("--wd", default=0.00005, type=float,
                         help="Weight decay")
+    parser.add_argument("--lr-decay", default=0.95, type=float, 
+                        help="Learning rate decay for Exponential scheduler")
     parser.add_argument("--expand-labels", action="store_true", 
                         help="expand labels to fit eval steps")
     parser.add_argument('--train-batch', default=64, type=int,
@@ -150,17 +222,28 @@ if __name__ == "__main__":
                         help="Number of workers to launch during training")
     parser.add_argument('--threshold', type=float, default=0.95,
                         help='Confidence Threshold for pseudo labeling')
-    parser.add_argument("--dataout", type=str, default="./runs/",
+    parser.add_argument("--dataout", type=str, default="./runs/{}".format(current_time),
                         help="Path to save log files")
     parser.add_argument("--model-depth", type=int, default=28,
                         help="model depth for wide resnet") 
     parser.add_argument("--model-width", type=int, default=2,
                         help="model width for wide resnet")
+    parser.add_argument("--checkpoint-freq", type=int, default=10, 
+                        help="Save checkpoint after these many epochs")
+    parser.add_argument("--resume", action="store_true")
+    parser.add_argument("--model-path", type=str)
+
+    
+
     
     # Add more arguments if you need them
     # Describe them in help
     # You can (and should) change the default values of the arguments
     
     args = parser.parse_args()
+
+    if args.resume and args.model_path is None:
+        parser.error("--resume requires --model-path."
+                    "If resuming from previous checkpoint, provide checkpoint path")
 
     main(args)
--- Task1_pseudoLabeling/utils.py
+++ Task1_pseudoLabeling/utils.py
@@ -1,4 +1,6 @@
 import torch
+from collections import OrderedDict
+from pathlib import Path
 
 def accuracy(output, target, topk=(1,)):
     """
@@ -21,3 +23,67 @@ def accuracy(output, target, topk=(1,)):
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
+def save_checkpoint(model, epoch, filename, optimiser=None, save_arch=False, params=None):
+    print('Saving checkpoint to {}'.format(filename))
+    attributes = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+    }
+
+    if optimiser is not None:
+        attributes['optimiser'] = optimiser.state_dict()
+
+    if save_arch:
+        attributes['arch'] = model
+
+    if params is not None:
+        attributes['params'] = params
+
+    path = Path(filename)
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
+    try:
+        torch.save(attributes, filename)
+    except TypeError:
+        if 'arch' in attributes:
+            print('Model architecture will be ignored because the architecture includes non-pickable objects.')
+            del attributes['arch']
+            torch.save(attributes, filename)
+
+
+def load_checkpoint(path, model=None, return_optimiser=False, return_params=False):
+    print('Loading checkpoint from {}'.format(path))
+    resume = torch.load(path)
+
+    rets = dict()
+
+    rets['epoch'] = resume['epoch']
+    if model is not None:
+        if ('module' in list(resume['state_dict'].keys())[0]) \
+                and not (isinstance(model, torch.nn.DataParallel)):
+            new_state_dict = OrderedDict()
+            for k, v in resume['state_dict'].items():
+                new_state_dict[k.replace('module.', '')] = v  # remove DataParallel wrapping
+
+            model.load_state_dict(new_state_dict)
+        else:
+            model.load_state_dict(resume['state_dict'])
+
+        rets['model'] = model
+
+    if return_optimiser:
+        rets['optimiser'] = resume['optimiser']
+    if return_params:
+        rets['params'] = resume['params']
+
+    return rets
+
+
+def load_model(path, model=None, is_inference=True):
+    resume = torch.load(path)
+    if model is None:
+        model = resume['arch']
+    model.load_state_dict(resume['state_dict'])
+    if is_inference:
+        model.eval()
+    return model
