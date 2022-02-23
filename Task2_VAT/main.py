0fcba094385c80431eee7d9fc924826ce8eeb99d
Suhas-G <suhas.g96.sg@gmail.com>
2022-02-23 12:50:27 +0100

    task2: change optimiser and hparams

--- Task2_VAT/main.py
+++ Task2_VAT/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import datetime
 import math
@@ -51,14 +52,18 @@ def get_params(args):
         'dataset': args.dataset,
         'num_labeled': args.num_labeled,
         'lr': args.lr,
-        'momentum': args.momentum,
         'weight_decay': args.wd,
-        'train_batch_size': args.train_batch,
+        'train_labelled_batch_size': args.train_labelled_batch,
+        'train_unlabelled_batch_size': args.train_unlabelled_batch,
         'test_batch_size': args.test_batch,
         'total_iter': args.total_iter,
         'iter_per_epoch': args.iter_per_epoch,
         'model_depth': args.model_depth,
-        'model_width': args.model_width
+        'model_width': args.model_width,
+        'vat_xi': args.vat_xi,
+        'vat_eps': args.vat_eps,
+        'vat_alpha': args.vat_alpha,
+        'vat_iter': args.vat_iter
     }
 
 def get_next_batch(dataset, loader, batch_size, num_workers, shuffle=True):
@@ -91,11 +96,11 @@ def main(args):
     writer = SummaryWriter(log_dir=args.dataout)
 
     labeled_loader      = iter(DataLoader(labeled_dataset, 
-                                    batch_size = args.train_batch, 
+                                    batch_size = args.train_labelled_batch, 
                                     shuffle = True, 
                                     num_workers=args.num_workers))
     unlabeled_loader    = iter(DataLoader(unlabeled_dataset, 
-                                    batch_size=args.train_batch,
+                                    batch_size=args.train_unlabelled_batch,
                                     shuffle = True, 
                                     num_workers=args.num_workers))
     test_loader         = DataLoader(test_dataset,
@@ -118,7 +123,7 @@ def main(args):
     # TODO: SUPPLY your code
     ############################################################################
 
-    optimiser = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimiser = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
     if args.resume:
         optimiser.load_state_dict(rets['optimiser'])
     criterion = torch.nn.CrossEntropyLoss()
@@ -128,14 +133,14 @@ def main(args):
         model.train()
         for i in range(args.iter_per_epoch):
             x_l, y_l, labeled_loader = get_next_batch(labeled_dataset, labeled_loader, 
-                                                        args.train_batch, args.num_workers)
+                                                        args.train_labelled_batch, args.num_workers)
             
             x_ul, _, unlabeled_loader = get_next_batch(unlabeled_dataset, unlabeled_loader, 
-                                                        args.train_batch, args.num_workers)
+                                                        args.train_unlabelled_batch, args.num_workers)
 
             # Combined labelled and unlabelled data. Choose batch size samples randomly.
             _combined_size = x_l.size()[0] + x_ul.size()[0]
-            sampling_indices = np.random.choice(_combined_size, min(args.train_batch, _combined_size), replace = False)
+            sampling_indices = np.random.choice(_combined_size, min(args.train_unlabelled_batch, _combined_size), replace = False)
             x_star = torch.cat((x_l, x_ul), dim=0)[sampling_indices]
             
             x_l, y_l    = x_l.to(device), y_l.to(device)
@@ -151,7 +156,7 @@ def main(args):
             outputs_labelled = model(x_l)
             loss_labelled = criterion(outputs_labelled, y_l)
 
-            total_loss = loss_labelled + args.vat_lambda * vat_loss
+            total_loss = loss_labelled + args.vat_alpha * vat_loss
             total_loss.backward()
             optimiser.step()
 
@@ -168,7 +173,14 @@ def main(args):
             save_checkpoint(model, epoch, './checkpoints/{}/checkpoint_{}.pth'.format(current_time,epoch + 1),
                             optimiser = optimiser, params=get_params(args))
 
+
     test_loss, test_accuracy = test(model, test_loader, criterion, device)
+
+    writer.add_hparams( get_params(args),
+                        {
+                            'final_validation_loss': validation_loss,
+                            'final_validation_accuracy': validation_accuracy,
+                        })
     writer.add_scalar('loss/test', test_loss, epoch)
     writer.add_scalar('accuracy/test', test_accuracy, epoch)
     writer.close()
@@ -186,10 +198,12 @@ if __name__ == "__main__":
                         default=4000, help='Total number of labeled samples')
     parser.add_argument("--expand-labels", action="store_true", 
                         help="expand labels to fit eval steps")
-    parser.add_argument('--train-batch', default=64, type=int,
-                        help='train batchsize')
+    parser.add_argument('--train-labelled-batch', default=64, type=int,
+                        help='Batchsize for labelled train data')
+    parser.add_argument('--train-unlabelled-batch', default=256, type=int,
+                        help='Batchsize for unlabelled train data')
     parser.add_argument('--test-batch', default=64, type=int,
-                        help='train batchsize')
+                        help='Batch size for test data')
     parser.add_argument('--num-workers', default=1, type=int,
                         help="Number of workers to launch during training")
 
@@ -200,21 +214,18 @@ if __name__ == "__main__":
                         help="model width for wide resnet")
 
     # Optimisation parameters
-    parser.add_argument("--lr", default=0.03, type=float, 
+    parser.add_argument("--lr", default=0.001, type=float, 
                         help="The initial learning rate") 
-    parser.add_argument("--momentum", default=0.9, type=float,
-                        help="Optimizer momentum")
-    parser.add_argument("--wd", default=0.00005, type=float,
+    parser.add_argument("--wd", default=0, type=float,
                         help="Weight decay")
-    parser.add_argument('--alpha', type=float, default=1.0, metavar='ALPHA',
-                        help='regularization coefficient (default: 0.01)')
+
 
     # Training Control parameters
-    parser.add_argument('--total-iter', default=1024*512, type=int,
+    parser.add_argument('--total-iter', default=128*375, type=int,
                         help='total number of iterations to run')
-    parser.add_argument('--iter-per-epoch', default=1024, type=int,
+    parser.add_argument('--iter-per-epoch', default=375, type=int,
                         help="Number of iterations to run per epoch")
-    parser.add_argument("--checkpoint-freq", type=int, default=10, 
+    parser.add_argument("--checkpoint-freq", type=int, default=20, 
                         help="Save checkpoint after these many epochs")
     parser.add_argument("--resume", action="store_true")
     parser.add_argument("--model-path", type=str)
@@ -226,17 +237,18 @@ if __name__ == "__main__":
     
 
     # VAT parameters
-    parser.add_argument("--vat-xi", default=10.0, type=float, 
+    parser.add_argument("--vat-xi", default=1, type=float, 
                         help="VAT xi parameter")
-    parser.add_argument("--vat-eps", default=1.0, type=float, 
+    parser.add_argument("--vat-eps", default=2.0, type=float, 
                         help="VAT epsilon parameter") 
-    parser.add_argument("--vat-lambda", default=1.0, type=float, 
-                        help="VAT lambda parameter") 
+    parser.add_argument('--vat-alpha', type=float, default=1.0, metavar='ALPHA',
+                        help='VAT regularization coefficient (default: 0.01)')
     parser.add_argument("--vat-iter", default=1, type=int, 
                         help="VAT iteration parameter") 
     
     args = parser.parse_args()
 
+    args.train_batch = args.train_labelled_batch
     if args.resume and args.model_path is None:
         parser.error("--resume requires --model-path."
                     "If resuming from previous checkpoint, provide checkpoint path")
